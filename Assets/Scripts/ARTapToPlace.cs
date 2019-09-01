2f2fc479ee7f31aa6412a97ba2d4a4cdb615d55f
davidbaindesign <davidbaindesign@gmail.com>
2019-09-01 16:43:27 -0400

    added to scene, tap to place script, updated project settings

---
+++ Assets/Scripts/ARTapToPlace.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+//using UnityEngine.Experimental.XR;
+using System;
+using UnityEngine.XR.ARSubsystems;
+
+public class ARTapToPlace : MonoBehaviour
+{
+    public GameObject placementIndicator;
+    private ARSessionOrigin arOrigin;
+    private Pose placementPose;
+    private ARRaycastManager arRaycastManager;
+    private bool placementPoseIsValid = false;
+
+    void Start()
+    {
+       // arOrigin = FindObjectOfType<ARSessionOrigin>();
+        arRaycastManager = FindObjectOfType<ARRaycastManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdatePlacementPose();
+        UpdatePlacementIndicator();
+    }
+
+    private void UpdatePlacementIndicator()
+    {
+        if (placementPoseIsValid)
+        {
+            placementIndicator.SetActive(true);
+            placementIndicator.transform.SetPositionAndRotation(placementPose.position, placementPose.rotation);
+        }
+        else
+        {
+            placementIndicator.SetActive(false);
+        }
+    }
+
+    private void UpdatePlacementPose()
+    {
+        Vector3 screenCenter = Camera.main.ViewportToScreenPoint(new Vector3(0.5f, 0.5f));
+        List<ARRaycastHit> hits = new List<ARRaycastHit>();
+       arRaycastManager.Raycast(screenCenter, hits, TrackableType.Planes);
+
+        Debug.Log(hits.Count);
+
+       placementPoseIsValid = hits.Count > 0;
+
+       if (placementPoseIsValid)
+        {
+            placementPose = hits[0].pose;
+        }
+    }
+}
